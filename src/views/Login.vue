4c4b1b2c6a04e11c71537d4bcf3a695bef410ef0
‘poyitu’ <2430820793@qq.com>
2023-03-02 16:50:58 +0800

    1

---
+++ src/views/Login.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <el-form :rulers="rules" :model="loginForm" class="loginContainer" ref="loginForm">
+      <h3 class="loginTitle">系统登录</h3>
+      <el-form-item prop="username">
+        <el-input
+          size="normal"
+          type="text"
+          v-model="loginForm.username"
+          auto-complete="off"
+          placeholder="请输入用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          size="normal"
+          type="password"
+          v-model="loginForm.password"
+          auto-complete="off"
+          placeholder="请输入密码"
+        ></el-input>
+      </el-form-item>
+      <el-checkbox class="loginRemember" v-model="checked"></el-checkbox>
+      <el-button size="normal" type="primary" style="width: 100%;" @click="submitLogin">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      checked: true,
+      loginForm: {
+        username: 'admin',
+        password: '123',
+        code:''
+      },
+      rules: {
+        username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+        password: [{required: true, message: '请输入密码', trigger: 'blur'}],
+      },
+    };
+  },
+  methods:{
+    submitLogin() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.loginContainer{
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  width: 350px;
+  padding: 15px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+.loginTitle {
+  margin: 15px auto 20px auto;
+  text-align: center;
+  color: #505458;
+}
+
+.loginRemember {
+  text-align: left;
+  margin: 0px 0px 15px 0px;
+}
+</style>
